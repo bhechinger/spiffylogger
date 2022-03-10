adaa34d9a01749c118d5f8de2d80f54387175741
bhechinger <wonko@4amlunch.net>
2022-03-10 23:52:00 +0000

    Trying to log to a bytes.Buffer for testing and can't make it work. Yet.

--- test/logger.go
+++ test/logger.go
@@ -1,19 +1,23 @@
 package test
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 
 	"github.com/bhechinger/spiffylogger"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 // TestableContext returns a context that has a byte buffer logger injected into it
 // Useful for capturing test output
 func TestableContext(level zapcore.Level) (context.Context, *bytes.Buffer) {
-	var buf bytes.Buffer
-	l := spiffylogger.NewLogger(level)
-	return spiffylogger.CtxWithLogger(context.Background(), l), &buf
+	var b bytes.Buffer
+	buf := bufio.NewWriter(&b)
+
+	l := spiffylogger.NewLogger(level, zap.ErrorOutput(zapcore.AddSync(buf)))
+	return spiffylogger.CtxWithLogger(context.Background(), l), &b
 }
 
 // LogCtx provides a context with the stdio logger
